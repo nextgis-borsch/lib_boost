a9d33c84e8c875b0919e69c344e27ec5f2f3ea2d
NikitaFeodonit <nikitafeodonit@yandex.com>
2016-05-24 23:28:23 +0300

    CMake files

---
+++ cmake/util.cmake
@@ -0,0 +1,57 @@
+################################################################################
+# Project:  CMake build scripts for Boost library
+# Purpose:  CMake build scripts
+# Author:   Dmitry Baryshnikov, dmitry.baryshnikov@nexgis.com
+# Author:   NikitaFeodonit, nfeodonit@yandex.com
+################################################################################
+# Copyright (C) 2016, NextGIS <info@nextgis.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+################################################################################
+
+
+function(check_version major minor rev)
+
+    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/boost/version.hpp VERSION_H_CONTENTS)
+
+    string(REGEX MATCH "BOOST_VERSION[ \t]+([0-9]+)"
+      BOOST_VERSION ${VERSION_H_CONTENTS})
+    string (REGEX MATCH "([0-9]+)"
+      BOOST_VERSION ${BOOST_VERSION})
+
+    math(EXPR MAJOR_VERSION "${BOOST_VERSION} / 100000")
+    math(EXPR MINOR_VERSION "${BOOST_VERSION} / 100 % 1000")
+    math(EXPR REV_VERSION "${BOOST_VERSION} % 100")
+
+    set(${major} ${MAJOR_VERSION} PARENT_SCOPE)
+    set(${minor} ${MINOR_VERSION} PARENT_SCOPE)
+    set(${rev} ${REV_VERSION} PARENT_SCOPE)
+
+endfunction(check_version)
+
+
+function(report_version name ver)
+
+    string(ASCII 27 Esc)
+    set(BoldYellow  "${Esc}[1;33m")
+    set(ColourReset "${Esc}[m")
+
+    message(STATUS "${BoldYellow}${name} version ${ver}${ColourReset}")
+
+endfunction()
